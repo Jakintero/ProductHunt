680d5728e6f5018b4c9dfcf8bd1de422512c7c77
Javier Quintero <jakintero@hotmail.com>
2017-10-27 15:39:12 -0500

    destroy comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,7 +17,7 @@
 
 class Product < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :votes
 
   has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>'}
