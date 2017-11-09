d9f1c839f7faa25636f6c347799183c0504c98c7
Javier Quintero <jakintero@hotmail.com>
2017-11-09 13:52:28 -0500

    Arreglando sombras del show

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -6,7 +6,7 @@ module ProductsHelper
 
   def header_style
     if @product.image.exists?
-      %{ style="background-image: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('#{@product.image.url}'); background-size: cover; background-position: center;" }.html_safe
+      %{ style="background-image: linear-gradient( rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25) ), url('#{@product.image.url}'); background-size: cover; background-position: center;" }.html_safe
     else
       ""
     end
